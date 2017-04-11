60bce4edcc64cd160de177ddc9730c6becde65a8
JY <https://github.com/Jiyunn/NaverMap>
2017-04-11 20:28:28 +0900

    20170411_home

---
+++ app/src/main/java/kr/happy/myarmy/SplashActivity.java
@@ -0,0 +1,29 @@
+package kr.happy.myarmy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+      getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+    }
+}
