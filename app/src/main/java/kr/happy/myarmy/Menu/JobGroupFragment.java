bdb2eb3d7f94e2e74022f3e290cffda4d7bec18b
JY <https://github.com/Jiyunn/NaverMap>
2017-04-15 01:17:37 +0900

    20170413

--- app/src/main/java/kr/happy/myarmy/Menu/JobGroupFragment.java
+++ app/src/main/java/kr/happy/myarmy/Menu/JobGroupFragment.java
@@ -3,18 +3,11 @@ package kr.happy.myarmy.Menu;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
 import kr.happy.myarmy.R;
-import kr.happy.myarmy.Recyclerview.ItemHome;
-import kr.happy.myarmy.Recyclerview.RecyclerAdapter;
 
 /**
  * Created by JY on 2017-04-11.
@@ -22,10 +15,7 @@ import kr.happy.myarmy.Recyclerview.RecyclerAdapter;
 
 public class JobGroupFragment extends Fragment {
 
-    private RecyclerView mRecyclerview;
-    private RecyclerAdapter adapter;
-    private LinearLayoutManager mLayoutManager;
-    private ArrayList<ItemHome> dataSet=new ArrayList<ItemHome>();
+
 
     public JobGroupFragment(){} //기본생성자
 
@@ -39,19 +29,9 @@ public class JobGroupFragment extends Fragment {
     @Override //뷰 생성
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        ViewGroup view=(ViewGroup)inflater.inflate(R.layout.home, container, false);
+        ViewGroup view=(ViewGroup)inflater.inflate(R.layout.jobgroup, container, false);
 
-        mRecyclerview=(RecyclerView) view.findViewById(R.id.rv_home);
-        mRecyclerview.setHasFixedSize(true);
 
-        mLayoutManager=new LinearLayoutManager(getActivity());
-        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        mRecyclerview.setLayoutManager(mLayoutManager);
-
-        adapter=new RecyclerAdapter(getActivity(), dataSet, R.layout.item_job); //어댑터 등록
-        mRecyclerview.setAdapter(adapter);
-
-        mRecyclerview.setItemAnimator(new DefaultItemAnimator());
 
         return view;
     }
